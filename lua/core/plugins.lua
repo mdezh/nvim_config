692525571acb08b1abd22d171565aede16eb64af
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-09-24 10:16:08 +0300

    add kanagawa theme

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,44 +1,46 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
-
 
 -- Example using a list of specs with the default options
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
 require("lazy").setup({
-    { 'navarasu/onedark.nvim' },
-    { 'phaazon/hop.nvim' },
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-        }
-    },
-    { 'nvim-treesitter/nvim-treesitter' },
-    { 'neovim/nvim-lspconfig' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/cmp-buffer' },
-    { 'hrsh7th/cmp-path' },
-    { 'hrsh7th/cmp-cmdline' },
-    { 'hrsh7th/nvim-cmp' },
-    { 'williamboman/mason.nvim' },
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.3',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    { 'jose-elias-alvarez/null-ls.nvim' },
+	-- themes
+	{ "navarasu/onedark.nvim" },
+	{ "rebelot/kanagawa.nvim" },
+	-- other
+	{ "phaazon/hop.nvim" },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+	},
+	{ "nvim-treesitter/nvim-treesitter" },
+	{ "neovim/nvim-lspconfig" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-cmdline" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "williamboman/mason.nvim" },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.3",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{ "jose-elias-alvarez/null-ls.nvim" },
 })
-
