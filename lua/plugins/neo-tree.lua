df37a91edf85805266de7e14773710facee0510b
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-09-24 23:22:54 +0300

    some fixes in neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -16,10 +16,12 @@ vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
 vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
 vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵 ", texthl = "DiagnosticSignHint" })
 
 require("neo-tree").setup({})
 
 vim.keymap.set("n", "<leader>E", ":Neotree float reveal<CR>")
 vim.keymap.set("n", "<leader>e", ":Neotree float focus<CR>")
+vim.keymap.set("n", "<leader><C-r>", ":Neotree left reveal<CR>")
+vim.keymap.set("n", "<leader><C-e>", ":Neotree left focus<CR>")
 vim.keymap.set("n", "<leader>o", ":Neotree float git_status<CR>")
