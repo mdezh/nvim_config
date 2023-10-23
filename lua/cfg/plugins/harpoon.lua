16dc733eb16fa586513cba3abe26d24acf7bcdad
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-10-23 23:45:53 +0300

    configure harpoon

--- lua/cfg/plugins/harpoon.lua
+++ lua/cfg/plugins/harpoon.lua
@@ -4,26 +4,41 @@ return {
 		"nvim-lua/plenary.nvim",
 	},
 	config = function()
-		-- set keymaps
+		require("harpoon").setup({
+			global_settings = {
+				-- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
+				save_on_toggle = false,
+
+				-- saves the harpoon file upon every change. disabling is unrecommended.
+				save_on_change = true,
+
+				-- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
+				enter_on_sendcmd = false,
+
+				-- closes any tmux windows harpoon that harpoon creates when you close Neovim.
+				tmux_autoclose_windows = false,
+
+				-- filetypes that you want to prevent from adding to the harpoon list menu.
+				excluded_filetypes = { "harpoon" },
+
+				-- set marks specific to each git branch inside git repository
+				mark_branch = false,
+
+				-- enable tabline with harpoon marks
+				tabline = false,
+				tabline_prefix = "   ",
+				tabline_suffix = "   ",
+			},
+		})
+
+		require("telescope").load_extension("harpoon")
+
 		local keymap = vim.keymap -- for conciseness
 
-		keymap.set(
-			"n",
-			"<leader>Hm",
-			"<cmd>lua require('harpoon.mark').add_file()<cr>",
-			{ desc = "Mark file with harpoon" }
-		)
-		keymap.set(
-			"n",
-			"<leader>Hn",
-			"<cmd>lua require('harpoon.ui').nav_next()<cr>",
-			{ desc = "Go to next harpoon mark" }
-		)
-		keymap.set(
-			"n",
-			"<leader>Hp",
-			"<cmd>lua require('harpoon.ui').nav_prev()<cr>",
-			{ desc = "Go to previous harpoon mark" }
-		)
+		keymap.set("n", "<leader>Hx", require("harpoon.mark").add_file, { desc = "Mark file with harpoon" })
+		keymap.set("n", "<leader>Hm", require("harpoon.ui").toggle_quick_menu, { desc = "Manipulate marks" })
+		keymap.set("n", "<leader>Hn", require("harpoon.ui").nav_next, { desc = "Go to next harpoon mark" })
+		keymap.set("n", "<leader>Hp", require("harpoon.ui").nav_prev, { desc = "Go to previous harpoon mark" })
+		keymap.set("n", "<leader>Hs", ":Telescope harpoon marks<CR>", { desc = "Select mark by Telescope" })
 	end,
 }
